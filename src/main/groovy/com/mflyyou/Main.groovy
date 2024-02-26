1ec444f6acbd0b6cc7ef190ccf52477ff3e62fdb
zhangpanqin <zhangpanqin@outlook.com>
2024-02-26 11:21:40 +0800

    11

---
+++ src/main/groovy/com/mflyyou/Main.groovy
@@ -0,0 +1,10 @@
+package com.mflyyou
+
+import com.mflyyou.test.Demo
+
+static void main(String[] args) {
+    println "Hello world!"
+    String aa="11"
+    def demo = new Demo()
+    demo.test()
+}
---
+++ src/main/groovy/com/mflyyou/test/Demo.java
@@ -0,0 +1,7 @@
+package com.mflyyou.test;
+
+public class Demo {
+    public void test() {
+        System.out.println("1111");
+    }
+}
